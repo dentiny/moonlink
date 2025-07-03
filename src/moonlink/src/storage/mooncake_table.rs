f340ac9d68d6e1f136c0484389ecdb51ea29d76f
dentiny <dentinyhao@gmail.com>
2025-07-03 16:51:26 -0700

    Revert "add checks for flush lsn 0 (#699)"
    This reverts commit 144e6587b5bac0e60b09c2d62c83c3a80b1ae24f.

--- src/moonlink/src/storage/mooncake_table.rs
+++ src/moonlink/src/storage/mooncake_table.rs
@@ -561,7 +561,7 @@ impl MooncakeTable {
         if contains_new_writes(iceberg_snapshot_res) {
             assert!(
                 self.last_iceberg_snapshot_lsn.is_none()
-                    || self.last_iceberg_snapshot_lsn.unwrap() < flush_lsn || flush_lsn == 0,
+                    || self.last_iceberg_snapshot_lsn.unwrap() < flush_lsn,
                 "Last iceberg snapshot LSN is {:?}, flush LSN is {:?}, imported data file number is {}, imported puffin file number is {}",
                 self.last_iceberg_snapshot_lsn,
                 flush_lsn,
