1a4adcdbf80880cb653af93a88293afb3013673c
dentiny <dentinyhao@gmail.com>
2025-07-08 09:57:14 +0000

    lint

--- src/moonlink/src/storage/filesystem/accessor/test_utils.rs
+++ src/moonlink/src/storage/filesystem/accessor/test_utils.rs
@@ -6,6 +6,6 @@ pub(crate) const TEST_CONTEST: &str = "helloworld";
 /// Test util function to create local file and write [`TEST_CONTEST`] to the destunation file (indicated by absolute path).
 pub(crate) async fn create_local_file(filepath: &str) {
     let mut file = tokio::fs::File::create(filepath).await.unwrap();
-    file.write(TEST_CONTEST.as_bytes()).await.unwrap();
+    let _ = file.write(TEST_CONTEST.as_bytes()).await.unwrap();
     file.flush().await.unwrap();
 }
