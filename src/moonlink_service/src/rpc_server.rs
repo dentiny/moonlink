ca66dfef64a31845dc2d30d8367b9051fb76c1fa
dentiny <dentinyhao@gmail.com>
2025-08-19 18:54:57 +0000

    semicolon

--- src/moonlink_service/src/rpc_server.rs
+++ src/moonlink_service/src/rpc_server.rs
@@ -165,7 +165,7 @@ where
                 files,
             } => {
                 backend.load_tables(database, table, files).await.unwrap();
-                write(&mut stream, &()).await?
+                write(&mut stream, &()).await?;
             }
         }
     }
