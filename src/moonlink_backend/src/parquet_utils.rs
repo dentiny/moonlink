a050b861d61d81d0a504e635b4ad553b24d037af
dentiny <dentinyhao@gmail.com>
2025-08-30 08:33:10 +0000

    use error

--- src/moonlink_backend/src/parquet_utils.rs
+++ src/moonlink_backend/src/parquet_utils.rs
@@ -15,9 +15,11 @@ const PARQUET_MAGIC: &[u8; 4] = b"PAR1";
 /// Get serialized uncompressed parquet metadata from the given local filepath.
 /// TODO(hjiang): Currently it only supports local filepath.
 pub(crate) async fn get_parquet_serialized_metadata(filepath: &str) -> Result<Vec<u8>> {
-    let mut file = tokio::fs::File::open(filepath)
-        .await
-        .map_err(|e| format!("Failed to open file {} with error {:?}", filepath, e))?;
+    let mut file = tokio::fs::File::open(&filepath).await.map_err(|e| {
+        Error::Io(ErrorStruct {
+            message: format!("Failed to open file {} with error {:?}", filepath, e),
+        })
+    })?;
 
     // Validate file size.
     let file_len = file.metadata().await?.len();
