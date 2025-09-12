c1b83c05736b282073606bf4df63e99f4e6e0061
dentiny <dentinyhao@gmail.com>
2025-09-12 02:29:11 +0000

    update table test

--- src/moonlink/src/storage/iceberg/glue_catalog_test.rs
+++ src/moonlink/src/storage/iceberg/glue_catalog_test.rs
@@ -1,5 +1,6 @@
 use crate::storage::filesystem::s3::s3_test_utils::*;
 use crate::storage::filesystem::s3::test_guard::TestGuard as S3TestGuard;
+use crate::storage::iceberg::catalog_test_impl::*;
 use crate::storage::iceberg::file_catalog_test_utils::get_test_schema;
 use crate::storage::iceberg::glue_catalog::GlueCatalog;
 use crate::storage::iceberg::iceberg_table_config::GlueCatalogConfig;
@@ -99,21 +100,19 @@ async fn create_glue_catalog(warehouse_uri: String) -> GlueCatalog {
     glue_catalog
 }
 
-#[tokio::test(flavor = "multi_thread", worker_threads = 2)]
-async fn test_table_creation_and_drop() {
-    // Use a unique bucket name for each test run to avoid conflicts
-    let (bucket_name, warehouse_uri) =
-        crate::storage::filesystem::s3::s3_test_utils::get_test_s3_bucket_and_warehouse();
-    let _guard = S3TestGuard::new(bucket_name.clone()).await;
-    let glue_catalog = create_glue_catalog(warehouse_uri.clone()).await;
-
-    let namespace_ident = NamespaceIdent::new(get_random_namespace());
+/// Test util function to create a namespace for the given glue catalog.
+async fn create_namespace(glue_catalog: &GlueCatalog, namespace_ident: NamespaceIdent) {
     glue_catalog
         .create_namespace(&namespace_ident, /*properties=*/ HashMap::new())
         .await
         .unwrap();
-    let table_ident = TableIdent::new(namespace_ident.clone(), get_random_table());
-
+}
+/// Test util function to create a table for the given glue catalog.
+async fn create_table(
+    glue_catalog: &GlueCatalog,
+    namespace_ident: NamespaceIdent,
+    table_ident: TableIdent,
+) {
     let table_creation = TableCreation::builder()
         .name(table_ident.name().to_string())
         .location(format!(
@@ -128,6 +127,19 @@ async fn test_table_creation_and_drop() {
         .create_table(&namespace_ident, table_creation)
         .await
         .unwrap();
+}
+
+#[tokio::test(flavor = "multi_thread", worker_threads = 2)]
+async fn test_table_creation_and_drop() {
+    let (bucket_name, warehouse_uri) =
+        crate::storage::filesystem::s3::s3_test_utils::get_test_s3_bucket_and_warehouse();
+    let _guard = S3TestGuard::new(bucket_name.clone()).await;
+    let glue_catalog = create_glue_catalog(warehouse_uri.clone()).await;
+
+    let namespace_ident = NamespaceIdent::new(get_random_namespace());
+    let table_ident = TableIdent::new(namespace_ident.clone(), get_random_table());
+    create_namespace(&glue_catalog, namespace_ident.clone()).await;
+    create_table(&glue_catalog, namespace_ident.clone(), table_ident.clone()).await;
 
     // Check table existence.
     let exists = glue_catalog.table_exists(&table_ident).await.unwrap();
@@ -147,5 +159,23 @@ async fn test_table_creation_and_drop() {
     glue_catalog.drop_namespace(&namespace_ident).await.unwrap();
 }
 
-#[tokio::test]
-async fn test_update_table() {}
+#[tokio::test(flavor = "multi_thread", worker_threads = 2)]
+async fn test_update_table() {
+    let (bucket_name, warehouse_uri) =
+        crate::storage::filesystem::s3::s3_test_utils::get_test_s3_bucket_and_warehouse();
+    let _guard = S3TestGuard::new(bucket_name.clone()).await;
+    let mut glue_catalog = create_glue_catalog(warehouse_uri.clone()).await;
+
+    let namespace_ident = NamespaceIdent::new(get_random_namespace());
+    let table_ident = TableIdent::new(namespace_ident.clone(), get_random_table());
+    // create_namespace(&glue_catalog, namespace_ident.clone()).await;
+
+    // Update table.
+    let namespace = namespace_ident.to_url_string();
+    let table = table_ident.name.clone();
+    test_update_table_impl(&mut glue_catalog, namespace, table).await;
+
+    // Drop namespace and table after test.
+    glue_catalog.drop_table(&table_ident).await.unwrap();
+    glue_catalog.drop_namespace(&namespace_ident).await.unwrap();
+}
