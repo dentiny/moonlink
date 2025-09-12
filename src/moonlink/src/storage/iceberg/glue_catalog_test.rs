db80466dd0ff8e9ec4fc348b6b01758dacf6aed7
dentiny <dentinyhao@gmail.com>
2025-09-12 02:53:10 +0000

    update schema

--- src/moonlink/src/storage/iceberg/glue_catalog_test.rs
+++ src/moonlink/src/storage/iceberg/glue_catalog_test.rs
@@ -179,3 +179,24 @@ async fn test_update_table() {
     glue_catalog.drop_table(&table_ident).await.unwrap();
     glue_catalog.drop_namespace(&namespace_ident).await.unwrap();
 }
+
+#[tokio::test(flavor = "multi_thread", worker_threads = 2)]
+async fn test_update_schema() {
+    let (bucket_name, warehouse_uri) =
+        crate::storage::filesystem::s3::s3_test_utils::get_test_s3_bucket_and_warehouse();
+    let _guard = S3TestGuard::new(bucket_name.clone()).await;
+    let mut glue_catalog = create_glue_catalog(warehouse_uri.clone()).await;
+
+    let namespace_ident = NamespaceIdent::new(get_random_namespace());
+    let table_ident = TableIdent::new(namespace_ident.clone(), get_random_table());
+    // create_namespace(&glue_catalog, namespace_ident.clone()).await;
+
+    // Update table.
+    let namespace = namespace_ident.to_url_string();
+    let table = table_ident.name.clone();
+    test_update_schema_impl(&mut glue_catalog, namespace, table).await;
+
+    // Drop namespace and table after test.
+    glue_catalog.drop_table(&table_ident).await.unwrap();
+    glue_catalog.drop_namespace(&namespace_ident).await.unwrap();
+}
